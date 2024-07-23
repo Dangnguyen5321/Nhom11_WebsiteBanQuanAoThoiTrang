f0af70b28057871290a94c527d02cf24815bba7f
Dangnguyen5321 <nguyenhaidang12cb3tqt@gmail.com>
2024-07-23 21:43:47 +0700

    Đã tạo project làm được 1 phần giao diện admin

--- Nhom11-WebsiteBanQuanAoThoiTrang/App_Start/RouteConfig.cs
+++ Nhom11-WebsiteBanQuanAoThoiTrang/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Nhom11_WebsiteBanQuanAoThoiTrang
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Nhom11_WebsiteBanQuanAoThoiTrang.Controllers" }
             );
         }
     }
