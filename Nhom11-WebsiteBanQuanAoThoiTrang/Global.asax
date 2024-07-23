adc25e4eb89bcdaaaabe243ba112ae56316684cf
Dangnguyen5321 <nguyenhaidang12cb3tqt@gmail.com>
2024-07-23 20:13:39 +0700

    Add project files.

---
+++ Nhom11-WebsiteBanQuanAoThoiTrang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nhom11_WebsiteBanQuanAoThoiTrang.MvcApplication" Language="C#" %>
---
+++ Nhom11-WebsiteBanQuanAoThoiTrang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nhom11_WebsiteBanQuanAoThoiTrang.Startup))]
+namespace Nhom11_WebsiteBanQuanAoThoiTrang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
